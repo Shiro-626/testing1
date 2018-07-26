0248ad2e427acc8567b6faac86ae94354d40dd62
Mabel Cao <10001009@student.toiohomai.ac.nz>
2018-07-26 13:53:05 +1200

    second

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -7,11 +7,18 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+
+            // output is the name of the string variable
             string output = "Hello World";
             Console.WriteLine(output);
 
-            Console.WriteLine("Hello World");
+            // \n creates a new line - same code from above on one line 
+            Console.WriteLine("\nHello World");
 
+            //prints the words inside the "" on screen
+            Console.WriteLine("\nPlease push enter to exit application");
+
+            //Reads the above code and stops the application from automatically closing
             Console.ReadLine();
         }
     }
